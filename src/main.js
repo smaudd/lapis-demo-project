c3e26ef8baaa949aebc90bdbdba003058803a3e5
Santiago <santiago@fragment.agency>
2020-04-30 12:04:16 +0200

    WIP DEMO

---
+++ src/main.js
@@ -0,0 +1,10 @@
+require.context('../content/', true, /\.yml$/)
+
+// if (module.hot) {
+//   module.hot.accept('./templates/index.ejs', function() {
+//     console.log('SOY TEMPLATE!!')
+//     // Do something with the updated library module...
+//   })
+// }
+
+console.log('soy sss')
