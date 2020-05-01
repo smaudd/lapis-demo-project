ee724e1002bb62a7d6c6cf98188b9112180e203c
santiago <smarcanoweb@gmail.com>
2020-05-01 21:10:22 +0200

    WIWIP

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 require.context('../content/', true, /\.yml$/)
+require.context('./templates/', true, /\.ejs$/)
+console.log(process)
+if (module.hot) {
+  module.hot.accept([`./templates/index.ejs`], function() {
+    console.log('SOY TEMPLATE!!')
+    // Do something with the updated library module...
+  })
+}
 
-// if (module.hot) {
-//   module.hot.accept('./templates/index.ejs', function() {
-//     console.log('SOY TEMPLATE!!')
-//     // Do something with the updated library module...
-//   })
-// }
-
-console.log('soy sss')
